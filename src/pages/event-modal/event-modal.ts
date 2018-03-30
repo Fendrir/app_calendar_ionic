832192f0067e713b29a3b9a4ff92e3bbc4cd317d
Fendrir <papao1@hotmail.fr>
2018-03-30 11:13:58 +0200

    test implant db sur event modal

--- src/pages/event-modal/event-modal.ts
+++ src/pages/event-modal/event-modal.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import * as moment from 'moment';
 
+// partie database firebase
+
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'; // remplace FirebaseListObservable
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 @IonicPage()
 @Component({
   selector: 'page-event-modal',
@@ -9,25 +15,37 @@ import * as moment from 'moment';
 })
 export class EventModalPage {
 
+  eventSourceDb: AngularFireList<any>;
+  eventsCal: Observable<any[]>;
+
   event = { startTime: new Date().toISOString(), endTime: new Date().toISOString(), allDay: false};
   minDate = new Date().toISOString();
 
   constructor(
     public navCtrl: NavController,
     private navParams: NavParams,
-    public viewCtrl: ViewController
+    public viewCtrl: ViewController,
+
+    db: AngularFireDatabase
   ) {
     let preselectedDate = moment(this.navParams.get('selectedDay')).format();
     this.event.startTime = preselectedDate;
     this.event.endTime = preselectedDate;
+
+    this.eventSourceDb = db.list('events');
+    this.eventsCal = this.eventSourceDb.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+
   }
 
   cancel() {
     this.viewCtrl.dismiss();
   }
 
-  save() {
-    this.viewCtrl.dismiss(this.event);
+  addEvent(newevent: string) {
+    // this.viewCtrl.dismiss(this.event);
+    this.eventSourceDb.push({ text: newevent });
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,65 +3,73 @@ import { NavController, ModalController, AlertController } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import * as moment from 'moment';
 
+// partie database firebase
+
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'; // remplace FirebaseListObservable
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
+
   eventSource = [];
-  viewTitle : string;
+  // eventSourceDb: AngularFireList<any[]>;
+  // eventsCal: Observable<any>;
+  viewTitle: string;
   selectedDay = new Date();
-  
+
   calendar = {
     mode: 'month',
     currentDate: this.selectedDay
   }
-  
+
   constructor(
     public navCtrl: NavController,
     public authProvider: AuthProvider,
     private modalCtrl: ModalController,
     private alertCtrl: AlertController
+    // db: AngularFireDatabase
   ) {
-    
+    // this.eventSourceDb = db.list('events');
+    // this.eventsCal = this.eventSourceDb.snapshotChanges().map(changes => {
+    //   return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    // });
   }
-  
+
   async logOut(): Promise<void> {
     await this.authProvider.logoutUser();
     this.navCtrl.setRoot('LoginPage');
   }
-  
+
   addEvent() {
-    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
+    let modal = this.modalCtrl.create('EventModalPage', { selectedDay: this.selectedDay });
     modal.present();
-    
+
     modal.onDidDismiss(data => {
       if (data) {
         let eventData = data;
-        
+
         eventData.startTime = new Date(data.startTime);
         eventData.endTime = new Date(data.endTime);
-        
+
         let events = this.eventSource;
         events.push(eventData);
         this.eventSource = [];
         setTimeout(() => {
           this.eventSource = events;
-
-          // test push database
-
-          // ... code
         });
-        
+
       }
     })
   }
-  
+
   onViewTitleChanged(title) {
     this.viewTitle = title;
   }
-  
+
   onTimeSelected(ev) {
     this.selectedDay = ev.selectedTime;
   }
@@ -69,9 +77,9 @@ export class HomePage {
   onEventSelected(event) {
     let start = moment(event.startTime).format('LLLL');
     let end = moment(event.endTime).format('LLLL');
-    
+
     let alert = this.alertCtrl.create({
-      title:'' + event.title,
+      title: '' + event.title,
       subTitle: 'From: ' + start + '<br>To: ' + end,
       buttons: ['OK']
     });
